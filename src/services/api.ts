3ff628ef478166abf3f9cc5885ab63dca965833f
REAMON\musud <wk.m.usuda0603@gmail.com>
2025-03-26 17:56:15 +0900

    u

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,8 @@ import {
 } from '../types/interfaces';
 
 // バックエンドAPIのエンドポイント
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://portfoliocreatebackend.onrender.com';
 
 // axiosインスタンスの設定
 const api = axios.create({
